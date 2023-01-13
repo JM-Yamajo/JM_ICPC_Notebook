5490ffa115362df4493219b8a35d363b567ea7e6
Jhonatan Martinez <70008088+JM-Yamajo@users.noreply.github.com>
2023-01-13 02:45:44 -0600

    Create FastPower.cpp

---
+++ Mate/FastPower.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long fastPower(int base, int exponent) {
+  
+    long long result = 1;
+  
+    while (exponent > 0) {
+      
+        if (exponent % 2 == 1) {
+            result = result * base;
+        }
+      
+        base = base * base;
+        exponent = exponent / 2;
+      
+    }
+  
+    return result;
+  
+}
+
+int main() {
+  
+    int base, exponent;
+  
+    cout << "Enter base: ";
+    cin >> base;
+  
+    cout << "Enter exponent: ";
+    cin >> exponent;
+  
+    cout << base << "^" << exponent << " = " << fastPower(base, exponent) << endl;
+  
+    return 0;
+  
+}
